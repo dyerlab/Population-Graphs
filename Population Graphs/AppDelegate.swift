49ff42dec826d8872a6158eac0b8b5423efbdfeb
dyerlab <rjdyer@vcu.edu>
2015-07-04 06:35:41 -0400

    Initial Commit

---
+++ Population Graphs/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Population Graphs
+//
+//  Created by Rodney Dyer on 7/4/15.
+//  Copyright © 2015 Rodney Dyer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Population Graphs/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Population Graphs
+//
+//  Created by Rodney Dyer on 7/4/15.
+//  Copyright © 2015 Rodney Dyer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
