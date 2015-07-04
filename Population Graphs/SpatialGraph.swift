9445147516136b990546bc5cfe12a074d4d9e3db
dyerlab <rjdyer@vcu.edu>
2015-07-04 13:54:46 -0400

    first commit

---
+++ Population Graphs/SpatialGraph.swift
@@ -0,0 +1,9 @@
+//
+//  SpatialGraph.swift
+//  Population Graphs
+//
+//  Created by Rodney Dyer on 7/4/15.
+//  Copyright © 2015 Rodney Dyer. All rights reserved.
+//
+
+import Foundation
---
+++ Population Graphs/SpatialObject.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialNode.swift
+//  Population Graphs
+//
+//  Created by Rodney Dyer on 7/4/15.
+//  Copyright © 2015 Rodney Dyer. All rights reserved.
+//
+
+import Foundation
+
+
+protocol SpatialObject {
+    var longitude: Double { get set }
+    var latitude: Double { get set }
+    
+}
+
